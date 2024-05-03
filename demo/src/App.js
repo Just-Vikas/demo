71db0119f6a8b1449fdb153063e0d01b4d3726da
Vikas Jain <vikas.jain@incture.com>
2024-05-03 15:38:03 +0530

    first commit

---
+++ demo/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Home';
+import Navbar from './Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+            <Navbar/>
+        </header>
+     
+   
+
+      <Home/>
+
+       
+    </div>
+  );
+}
+
+export default App;
